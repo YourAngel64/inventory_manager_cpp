6cf0518047e04e75ff61ae849d682a3862c868fc
Enten <angelyadiel8776@gmail.com>
2024-02-23 21:44:18 -0600

    Add Basic files to start project; Wrote algorithm organization

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+/*
+Inventory System Algorithm
+Algorithm Goal: Create an inventory system (at least start it). To manage an inventory of a company that sells video games
+Algorithm Blueprint:
+
+-Purchase:
+Add item to inventory. Details include, Information about purchased object and price of it. And pruchased quantity
+
+-Sell:
+Sell item to inventory. Details of the object being sold. Quantity being sold, etc
+
+-print details:
+Print details on the items available, the information, quantity available, profit so far, etc
+
+Details to learn in the way:
+-How to make console interactive with arrow keys
+-Learn how file management in c works
+-Learn extra details for classes
+
+Goals:
+-master the use of classes and information organization of c++
+-Have something cool for portfolio
+-It may be useful for any future businesses you may have
+
+Extra details:
+-When program is functionable, will make a documentation for it and upload it to github
+-It may take u days to finish but dw cause ur learning bro
+-before starting project write DETAIL BY DETAIL of each step so you don't get lost
+*/
+
+int main(){
+
+
+
+    return 0;
+}
