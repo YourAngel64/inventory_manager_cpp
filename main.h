ee69df48721fb318c1d830e20f0e33555213d298
YourAngel <angelyadiel8776@gmail.com>
2024-03-04 18:57:24 -0600

    Fixed little mistake

--- main.h
+++ main.h
@@ -51,7 +51,7 @@ int* option(){
 //function that deals with saving and reading inventory items
 void inventory_file(){
     fstream file;
-    file = use_file("Inventory.txt", "read");
+    file = use_file("Inventory.txt", "write");
 
     file << "\n";
     file << "Name: " << purchase_product.name << endl;
